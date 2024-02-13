9693ad581cf07bbede6d7c8ca6889ba0bf2b384b
ToniGrbic <tonigrbic.5@gmail.com>
2024-02-13 19:37:48 +0100

    removed proxy

--- scripts/api.js
+++ scripts/api.js
@@ -1,19 +1,12 @@
 const baseUrl = "https://api.rawg.io/api";
-const proxyUrl = "https://cors-anywhere.herokuapp.com";
+//const proxyUrl = "https://cors-anywhere.herokuapp.com";
 const API_KEY = "464bc085dbbf4f33bcb2ccb39d36a6ec";
 const keyParam = new URLSearchParams(`key=${API_KEY}`);
 
 async function getGames() {
   try {
     const response = await fetch(
-      `${proxyUrl}/${baseUrl}/games?${keyParam}&ordering=metacritic-released`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-        },
-      }
+      `${baseUrl}/games?${keyParam}&ordering=metacritic-released`
     );
     const data = await response.json();
     console.log(data);
