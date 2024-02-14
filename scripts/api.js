0bab289d05539269efc9820d256919408eab16b8
ToniGrbic <tonigrbic.5@gmail.com>
2024-02-14 01:49:24 +0100

    refactoring and zdk3

--- scripts/api.js
+++ scripts/api.js
@@ -2,12 +2,13 @@ const baseUrl = "https://api.rawg.io/api";
 //const proxyUrl = "https://cors-anywhere.herokuapp.com";
 const API_KEY = "464bc085dbbf4f33bcb2ccb39d36a6ec";
 
-async function getTopRatedGames() {
-  const searchParams = new URLSearchParams(`key=${API_KEY}`);
-  searchParams.append("ordering", "metacritic-released");
+function keyParam() {
+  return new URLSearchParams(`key=${API_KEY}`);
+}
 
+async function fetchData(url) {
   try {
-    const response = await fetch(`${baseUrl}/games?${searchParams}`);
+    const response = await fetch(url);
     const data = await response.json();
     console.log(data);
     return data.results;
@@ -15,21 +16,47 @@ async function getTopRatedGames() {
     console.error(error);
   }
 }
+async function getTopRatedGames() {
+  const searchParams = keyParam();
+  searchParams.append("ordering", "-metacritic");
+
+  const games = await fetchData(`${baseUrl}/games?${searchParams}`);
+  return games;
+}
 
 async function getGamesBySearchTerm(searchTerm = "GTA") {
-  const searchParams = new URLSearchParams(`key=${API_KEY}`);
+  const searchParams = keyParam();
   searchParams.append("search", searchTerm);
   searchParams.append("page_size", "10");
   searchParams.append("ordering", "released");
 
-  try {
-    const response = await fetch(`${baseUrl}/games?${searchParams}`);
-    const data = await response.json();
-    console.log(data);
-    return data.results;
-  } catch (error) {
-    console.error(error);
-  }
+  const games = await fetchData(`${baseUrl}/games?${searchParams}`);
+  return games;
 }
 
-export { getTopRatedGames, getGamesBySearchTerm };
+async function getGamesByPlatform(platformIds) {
+  const searchParams = keyParam();
+  searchParams.append("platforms", platformIds);
+  searchParams.append("page_size", "20");
+  searchParams.append("metacritic", "80,100");
+  searchParams.append("ordering", "name");
+
+  const games = await fetchData(`${baseUrl}/games?${searchParams}`);
+  return games;
+}
+
+async function getPlatforms() {
+  const searchParams = keyParam();
+  searchParams.append("page_size", "10");
+  searchParams.append("ordering", "-games_count");
+
+  const platforms = await fetchData(`${baseUrl}/platforms?${searchParams}`);
+  return platforms;
+}
+
+export {
+  getTopRatedGames,
+  getGamesBySearchTerm,
+  getPlatforms,
+  getGamesByPlatform,
+};
